a380b7db18294787cefdde363538cafa7b48f385
Ruairi-Osul <rorylikesmusic@gmail.com>
2021-10-29 14:26:56 -0400

    added docstring for 2 group

--- df_grouping_utils/multiple_df_groupby.py
+++ df_grouping_utils/multiple_df_groupby.py
@@ -10,6 +10,18 @@ def groupby_two_df(
     df2_group_colname: str = "group",
     **kwargs
 ) -> Union[pd.Series, pd.DataFrame]:
+    """Apply split apply combine to two pandas DataFrames
+
+    Args:
+        df1 (pd.DataFrame): First DataFrame
+        df2 (pd.DataFrame): Second DataFrame
+        f (Callable): Callable that takes two DataFrames as input
+        df1_group_colname (str, optional): Label of the group column in df1. Defaults to "group".
+        df2_group_colname (str, optional): Label of the group column in df2. Defaults to "group".
+
+    Returns:
+        Union[pd.Series, pd.DataFrame]: The results of the group by
+    """
     groups_df1 = set(df1[df1_group_colname].unique())
     groups_df2 = set(df2[df2_group_colname].unique())
     groups_shared = groups_df1.intersection(groups_df2)
