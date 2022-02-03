2a8d10fcbd1293cc91b47706e439fc28dd5913cb
Ruairi-Osul <rorylikesmusic@gmail.com>
2022-02-03 09:46:37 -0500

    fixed pandas warning

--- df_grouping_utils/multiple_df_groupby.py
+++ df_grouping_utils/multiple_df_groupby.py
@@ -22,16 +22,18 @@ def groupby_two_df(
     Returns:
         Union[pd.Series, pd.DataFrame]: The results of the group by
     """
-    groups_df1 = set(df1[df1_group_colname].unique())
-    groups_df2 = set(df2[df2_group_colname].unique())
+    groups_df1 = set(
+        df1[df1_group_colname].unique()
+    ).copy()  # so not updating the original dataframes
+    groups_df2 = set(
+        df2[df2_group_colname].unique()
+    ).copy()  # so not updating the original dataframes
     groups_shared = groups_df1.intersection(groups_df2)
     results: List[pd.DataFrame] = []
     for group in groups_shared:
         df1_sub = df1.loc[df1[df1_group_colname] == group]
         df2_sub = df2.loc[df2[df2_group_colname] == group]
-        group_result = f(
-            df1_sub, df2_sub, **kwargs
-        ).copy()  # so not updating the original dataframes
+        group_result = f(df1_sub, df2_sub, **kwargs)
         group_result["group"] = group
         results.append(group_result)
     return pd.concat(results)
